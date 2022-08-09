0f67272dd8a1b175e90d397e83a1eba7e59698aa
Nguyen Chanh <nguyenchanh0306@gmail.com>
2022-08-09 22:17:39 +0700

    feat: Create UI menu

--- src/components/Poppers/Menu/index.js
+++ src/components/Poppers/Menu/index.js
@@ -1,14 +1,37 @@
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
+
 import { Wrapper as PopperWrapper } from '~/components/Poppers';
 import MenuItem from './menu-items';
+import Header from './header';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children, items }) {
+const defaultFunc = () => {};
+
+function Menu({ children, items = [], onChange = defaultFunc }) {
+  const [history, setHistory] = useState([{ data: items }]);
+  const current = history[history.length - 1];
+
   const renderItems = () => {
-    return items && items.map((item, i) => <MenuItem key={i} item={item} />);
+    return current.data.map((item, i) => {
+      return <MenuItem key={i} item={item} to={item.to} onClick={() => handleClickMenu(item)} />;
+    });
+  };
+
+  const handleClickMenu = (item) => {
+    const isParent = !!item.children;
+    if (isParent) {
+      setHistory((prev) => [...prev, item.children]);
+    } else {
+      onChange(item);
+    }
+  };
+
+  const handleBackMenu = () => {
+    setHistory((prev) => prev.slice(0, history.length - 1));
   };
 
   return (
@@ -18,7 +41,10 @@ function Menu({ children, items }) {
       placement="bottom-end"
       render={(attrs) => (
         <div className={cx('content')} tabIndex="-1" {...attrs}>
-          <PopperWrapper className={cx('menu-items')}>{renderItems()}</PopperWrapper>
+          <PopperWrapper className={cx('menu-items')}>
+            {history.length > 1 && <Header title="Language" onBack={handleBackMenu} />}
+            {renderItems()}
+          </PopperWrapper>
         </div>
       )}
     >
--- src/components/Poppers/Menu/menu-items.js
+++ src/components/Poppers/Menu/menu-items.js
@@ -4,9 +4,9 @@ import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ item }) {
+function MenuItem({ item, onClick, to }) {
   return (
-    <Button className={cx('btn-menu-item')} leftIcon={item.icon} noneBorder>
+    <Button className={cx('btn-menu-item')} to={to} leftIcon={item.icon} noneBorder onClick={onClick}>
       {item.title}
     </Button>
   );
